23de5e7a2bae684ef0d335dabe5571043d678d7f
Jade Meskill <jade.meskill@gmail.com>
2015-05-10 13:06:46 -0400

    set whitelist

--- js/master.js
+++ js/master.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   conn.on('ready', function(data){
     console.log('Ready', data);
     data.type = 'octoblu:kiosk';
+    data.discoverWhitelist = [data.uuid];
     conn.update(data);
     $('.save-url').text('https://kiosk.octoblu.com/' + data.uuid + '/' + data.token);
     conn.on('error', console.log);
