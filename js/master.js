cbead611e48540dbda5517295199897f6eab43ad
Jade Meskill <jade.meskill@gmail.com>
2015-05-10 13:05:11 -0400

    update type as octoblu:kiosk

--- js/master.js
+++ js/master.js
@@ -2,14 +2,9 @@ $(document).ready(function(){
   var conn = meshblu.createConnection({});
   conn.on('ready', function(data){
     console.log('Ready', data);
-    conn.subscribe({uuid: '3f05c000-dd6e-11e4-bea4-6baf4218425e'}, function(data){
-      console.log('subscribe', data);
-    });
+    data.type = 'octoblu:kiosk';
+    conn.update(data);
+    $('.save-url').text('https://kiosk.octoblu.com/' + data.uuid + '/' + data.token);
     conn.on('error', console.log);
-    conn.on('message', function(msg){
-      var winner = msg.winner;
-      $('.winner').text('@' + winner.screen_name);
-      $('.chicken-dinner').text(winner.text);
-    });
   });
 })
