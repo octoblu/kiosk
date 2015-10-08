644110a5a21bf1965d7db718fcb27f1b8d010e08
Sqrt of Saturn <sqrt@octoblu.com>
2015-10-08 13:39:08 -0700

    added text and html support

--- js/kiosk.js
+++ js/kiosk.js
@@ -4,34 +4,73 @@ var connection = meshblu.createConnection({
   uuid: location.hash.substring(2).split('/')[0],
   token: location.hash.substring(2).split('/')[1]
 });
-var currentUrl;
+
+var currentUrl, currentText, currentHTML;
+
 
 connection.on('ready', function(data){
   console.log('ready');
+  $('#text').fitText();
+
   connection.on('message', function(message){
-    var url = message.url;
-    if(!url){
-      var payload = message.payload || {};
-      url = payload.url;
-    }
-    updateKiosk(url);
+    var object = {};
+    object.url = getProperty(message, 'url');
+    object.text = getProperty(message, 'text');
+    object.textStyles = getProperty(message, 'textStyles');
+    object.html = getProperty(message, 'html');
+    updateKiosk(object);
   });
   connection.whoami({}, function(result){
     result = result || {};
     if(result.error){
       return console.log('whoami error', result);
     }
-    var url = result.url;
-    if(!currentUrl && url){
-      updateKiosk(url);
-    }
+    updateKiosk(result);
   });
 });
 
+function getProperty(message, key){
+  var value = message[key];
+  if(!value){
+    var payload = message.payload || {};
+    value = payload[key];
+  }
+  return value;
+}
 
-function updateKiosk(url){
-  currentUrl = url;
-  console.log('Opening URL', url);
-  $('#default-page').hide();
-  $('iframe').attr('src', url);
+function updateKiosk(object){
+  object = object || {};
+  object.textStyles = {};
+  $('#text').hide();
+  $('#text').empty();
+  $('#text').attr('styles', '');
+  $('#html').hide();
+  $('#html').empty();
+
+  if(object.url && currentUrl !== object.url){
+    console.log('Opening URL', object.url);
+    currentUrl = object.url;
+    $('#default-page').hide();
+    $('#iframe').attr('src', object.url);
+    return;
+  }
+
+  if(object.text && currentText !== object.text){
+    console.log('Showing Text', object.text, object.textStyles);
+    currentText = object.text;
+    $('#default-page').hide();
+    $('#text').show();
+    $('#text').text(object.text);
+    $('#text').css(object.textStyles);
+    return;
+  }
+
+  if(object.html && currentHTML !== object.html){
+    console.log('Showing HTML', object.html);
+    currentHTML = object.html;
+    $('#default-page').hide();
+    $('#html').show();
+    $('#html').html(object.html);
+    return;
+  }
 }
